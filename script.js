445f93472226aef0037329728dbcf92dad0b87d8
Ahmedabser <ahmedabser6060@gmail.com>
2025-01-26 17:01:05 +0200

    finish

--- script.js
+++ script.js
@@ -7,31 +7,31 @@ function displayDay() {
     let day=" ";
 
 
-    if (date==" "){
-        day= "invalid input";
-    }
-    else if (date==1){
-        day="الأحد";
-    }else if(date==2){
-        day="الإثنين";
-    }else if(date==3){
-        day="الثلاثاء";
-    }else if(date==4){
-        day="الأربعاء";
-    }else if(date==5){
-        day="الخميس";
-    }else if(date==6){
-        day="الجمعة";}
-    else if(date==7){
-        day="السبت";
-    }
-    else{
-        day= "invalid input";
+    if (date.trim() === "") {
+        day = "invalid input";
+    } else if (date == 1) {
+        day = "Sunday";
+    } else if (date == 2) {
+        day = "Monday";
+    } else if (date == 3) {
+        day = "Tuesday";
+    } else if (date == 4) {
+        day = "Wednesday";
+    } else if (date == 5) {
+        day = "Thursday";
+    } else if (date == 6) {
+        day = "Friday";
+    } else if (date == 7) {
+        day = "Saturday";
+    } else {
+        day = "invalid input";
     }
     // عرض اليوم في العنصر #dateOutput
     document.getElementById("dateOutput").innerHTML = day;
   }
+
   
+// Action for Enter key
 document.getElementById("date").addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         displayDay();
